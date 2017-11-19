2bfdbf7308359371a7f8ae90409f881837470887
RafLtd <33802781+RafLtd@users.noreply.github.com>
2017-11-19 23:27:02 +0200

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,7 +1,6 @@
 var my_string = prompt('Say me yes');
 
 function string_sort(my_string){
-alert( checkSpam(str) );
 my_string = my_string.toLowerCase();
 var arr = my_string.split(/[^\wа-яё]+/gi);
 return arr;
